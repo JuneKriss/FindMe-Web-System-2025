0bffa639688ecc9bbd186b35ca0414b7c62ba081
JuneKriss <junekriss.avanzado@gmail.com>
2025-08-23 21:48:27 +0800

    Initial commit

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.login, name='login'),
+
+    path('dashboard/', views.dashboard, name='dashboard'),
+
     path('api/ping/', views.ping, name='ping'),          # simple JSON endpoint
 ]
--- core/views.py
+++ core/views.py
@@ -6,5 +6,8 @@ from django.utils import timezone
 def login(request):
     return render(request, 'login.html')
 
+def dashboard(request):
+    return render(request, 'dashboard.html')
+
 def ping(request):
     return JsonResponse({'ok': True, 'time': timezone.now().isoformat()})
